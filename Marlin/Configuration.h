eae5296a2aff410738628fa53649b98bc45e6049
Christian Stewart <christian@aperture.us>
2023-07-10 21:01:12 -0700

    ender3: pid tune the bed
    Run auto-tune for the bed PID:
    
      // "M303 E-1 C8 S60 U1" to run autotune for 8 cycles.
    
    Recv: #define DEFAULT_bedKp 253.46
    Recv: #define DEFAULT_bedKi 49.50
    Recv: #define DEFAULT_bedKd 865.14
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,9 +753,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 273.01
+  #define DEFAULT_bedKi 53.32
+  #define DEFAULT_bedKd 931.86
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
@@ -811,8 +811,8 @@
   #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 
-  //#define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of flash)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of flash)
+  #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of flash)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of flash)
 #endif
 
 // @section safety
